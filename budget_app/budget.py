e6fa2b17c731b0c40f14dfd65e292427c93f6f2d
Angel Todorov <angel.k.todorov@gmail.com>
2023-03-04 10:49:41 +0200

    added string representation of the Category class

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -6,14 +6,25 @@ class Category:
       self.balance = 0
 
     def __str__(self):
-      return 'TBD'
+      line_length = 30
+      header_line = f"{int(((line_length - len(self.category)) / 2)) * '*'}{self.category}"
+      header_line = header_line.ljust(30, '*')
+
+      item_line = ''
+      for item in self.ledger:
+         description_line = f"{item['description']}".ljust(23)
+         amount_line = f"{item['amount']}".rjust(7)
+         item_line = f"{item_line}{description_line}{amount_line}\n"
+
+      totals_line = f'Total: {float(self.get_balance())}'  
+      return f'{header_line}\n{item_line}{totals_line}'
     
     def check_funds(self, check_amount):
        if check_amount > self.balance: return False
        else: return True
     
     def deposit(self, deposit_amount, deposit_description = ''): 
-      deposit_entry = {'amount': deposit_amount, 
+      deposit_entry = {'amount': float(deposit_amount), 
                        'description': deposit_description}
       
       self.balance += deposit_amount
@@ -22,7 +33,7 @@ class Category:
     def withdraw(self, withdraw_amount, withdraw_description = ''):
        if not self.check_funds(withdraw_amount): return False
        else:
-          withdraw_entry = {'amount': -withdraw_amount, 
+          withdraw_entry = {'amount': float(-withdraw_amount), 
                             'description': withdraw_description}
           self.balance -= withdraw_amount
           self.ledger.append(withdraw_entry)
