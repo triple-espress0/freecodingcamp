1996a8c32e7dc784e3281859230d13a7f112245e
Angel Todorov <angel.k.todorov@gmail.com>
2023-03-08 21:42:53 +0200

    final solution

--- probability_calculator/main.py
+++ probability_calculator/main.py
@@ -3,13 +3,13 @@ import prob_calculator
 from unittest import main
 
 prob_calculator.random.seed(95)
-hat = prob_calculator.Hat(blue=4, red=2, green=6)
+hat = prob_calculator.Hat(blue=3, red=2, green=6)
 probability = prob_calculator.experiment(
     hat=hat,
     expected_balls={"blue": 2,
-                    "red": 1},
+                    "green": 1},
     num_balls_drawn=4,
-    num_experiments=3000)
+    num_experiments=1000)
 print("Probability:", probability)
 
 # Run unit tests automatically
--- probability_calculator/prob_calculator.py
+++ probability_calculator/prob_calculator.py
@@ -1,15 +1,46 @@
 import copy
 import random
-# Consider using the modules imported above.
+
 
 class Hat:
-    def __init__(self, **balls):
-        self.contents = list()
-        for ball in balls:
-            for i in range(int(balls[ball])):
-                self.contents.append(ball)
 
-        print(self.contents)
+  def __init__(self, **balls):
+    self.contents = list()
+    for ball in balls:
+      for i in range(int(balls[ball])):
+        self.contents.append(ball)
 
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    return 'foo'
+  def draw(self, quant_balls):
+    if quant_balls >= len(self.contents):
+      self.contents.clear
+      return self.contents
+    else:
+      drawn_balls = list()
+
+      for i in range(quant_balls):
+        rand = random.randint(0, len(self.contents) - 1)
+        drawn_balls.append(self.contents[rand])
+        del self.contents[rand]
+
+      return drawn_balls
+
+
+def experiment(hat: 'Hat', expected_balls, num_balls_drawn, num_experiments):
+
+  succes_count = 0
+
+  for i in range(num_experiments):
+    hat_copy = copy.deepcopy(hat)
+    drawn_balls = hat_copy.draw(num_balls_drawn)
+
+    is_subset = False
+    for colour in expected_balls:
+      if expected_balls[colour] > drawn_balls.count(colour):
+        is_subset = False
+        break
+      else:
+        is_subset = True
+
+    if is_subset: succes_count += 1
+
+  return succes_count / num_experiments
